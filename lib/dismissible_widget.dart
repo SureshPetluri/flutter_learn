ce454ac083f34760fe8557468ffd6a723eb5b4f1
Dinesh Battula <dinesh.battula@isowhiz.in>
2023-09-15 12:14:23 +0530

    added dismissible widget

---
+++ lib/dismissible_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<String> items = List.generate(10, (index) => 'Item $index');
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dismissible Example'),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Dismissible(
+              key: Key(item), // Unique key for each item
+              confirmDismiss: (DismissDirection direction) async {
+                // Show a confirmation dialog before dismissing
+                final bool shouldDismiss = await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog.adaptive(
+                      title: Text('Confirm Delete'),
+                      content: Text('Are you sure you want to delete $item?'),
+                      actions: <Widget>[
+                        ElevatedButton(
+                          child: Text('Cancel'),
+                          onPressed: () {
+                            Navigator.of(context).pop(false); // Don't dismiss
+                          },
+                        ),
+                        ElevatedButton(
+                          child: Text('Delete'),
+                          onPressed: () {
+                            Navigator.of(context).pop(true); // Dismiss
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+                return shouldDismiss;
+              },
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.only(right: 20.0),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+              onDismissed: (direction) {
+                // Delete the item only if it was confirmed in the dialog
+                if (direction == DismissDirection.endToStart) {
+                  items.removeAt(index);
+                  // Scaffold.of(context).ScaffoldState(
+                  //   SnackBar(
+                  //     content: Text('$item deleted'),
+                  //   ),
+                  // );
+                }
+              },
+              child: ListTile(
+                title: Text(item),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
